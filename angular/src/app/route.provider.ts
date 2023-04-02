5fb00bd7a90202f85f3d301e10d5d76e1bdafdbc
Stano Peťko <stano@stanopetko.eu>
2023-04-02 11:58:36 +0200

    Implement UI for book list

--- angular/src/app/route.provider.ts
+++ angular/src/app/route.provider.ts
@@ -5,14 +5,27 @@ export const APP_ROUTE_PROVIDER = [
   { provide: APP_INITIALIZER, useFactory: configureRoutes, deps: [RoutesService], multi: true },
 ];
 
-function configureRoutes(routesService: RoutesService) {
+function configureRoutes(routes: RoutesService) {
   return () => {
-    routesService.add([
+    routes.add([
       {
         path: '/',
         name: '::Menu:Home',
         iconClass: 'fas fa-home',
         order: 1,
+        layout: eLayoutType.application,
+      },
+      {
+        path: '/book-store',
+        name: '::Menu:BookStore',
+        iconClass: 'fas fa-book',
+        order: 2,
+        layout: eLayoutType.application,
+      },
+      {
+        path: '/books',
+        name: '::Menu:Books',
+        parentName: '::Menu:BookStore',
         layout: eLayoutType.application,
       },
     ]);
